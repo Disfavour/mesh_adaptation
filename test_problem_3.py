c2ce5fd71ce955869efad1eb7019f1475728a2fc
Disfavour <disfavour228@gmail.com>
2025-05-06 15:25:09 +0300

    upd

--- test_problem_3.py
+++ test_problem_3.py
@@ -14,51 +14,38 @@ import adaptation_v2
 import plotting
 
 
-def solve_test_problem(mesh_fname):
+def solve_test_problem(mesh_fname, d=0.1):
     domain, cell_markers, facet_markers = dolfinx.io.gmshio.read_from_msh(mesh_fname, MPI.COMM_WORLD, gdim=2)
-
-    Q = dolfinx.fem.functionspace(domain, ("DG", 0))
-    k = dolfinx.fem.Function(Q)
-
-    k1, k2 = 1, 10
-
-    inner_circle_tag = 1
-    inner_circle_cells = cell_markers.find(inner_circle_tag)
-    k.x.array[inner_circle_cells] = np.full_like(inner_circle_cells, k1, dtype=dolfinx.default_scalar_type)
-
-    outer_circle_tag = 2
-    outer_circle_cells = cell_markers.find(outer_circle_tag)
-    k.x.array[outer_circle_cells] = np.full_like(outer_circle_cells, k2, dtype=dolfinx.default_scalar_type)
-
-    #show_markers(domain, cell_markers)
 
     V = dolfinx.fem.functionspace(domain, ("Lagrange", 1))
     u = ufl.TrialFunction(V)
     v = ufl.TestFunction(V)
 
-    # f = - ufl.div(k * ufl.grad(u_ufl))
-    # a = ufl.dot(k * ufl.grad(u), ufl.grad(v)) * ufl.dx
-    # L = f * v * ufl.dx + ufl.dot(k*ufl.grad(u_ufl), n) * v * ufl.ds
-
-    Neumann_condition = 0
     Dirichlet_condition = 0
-    f = 1
+    a = 1
+    #d = 0.1 / 4
+    #f = a / d * ufl.exp(- (x - x_0) ** 2 / d ** 2
 
-    Neumann_condition = dolfinx.fem.Constant(domain, PETSc.ScalarType(Neumann_condition))
-    #Dirichlet_condition = dolfinx.fem.Constant(domain, PETSc.ScalarType(Dirichlet_condition))
-    f = dolfinx.fem.Constant(domain, PETSc.ScalarType(f))
+    #f = dolfinx.fem.Constant(domain, dolfinx.default_scalar_type(-6))
 
-    a = ufl.dot(k * ufl.grad(u), ufl.grad(v)) * ufl.dx
-    L = f * v * ufl.dx  + Neumann_condition * v * ufl.ds
+    x = ufl.SpatialCoordinate(domain)
+    x_0 = ufl.as_vector([-1, -0.5])
+    f =  a/d * ufl.exp(-(x-x_0)**2 / d**2)
 
-    def boundary_Dirichlet(x):
-        x = x.T
-        #return np.isclose(np.linalg.norm(x, axis=1), 3)
-        return np.logical_and(np.isclose(np.linalg.norm(x, axis=1), 3), x[:, 1] > x[:, 0] - 3 + 1e-3)
+    
+
+    a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx + u*v * ufl.dx
+    L = f * v * ufl.dx
+
+    tdim = domain.topology.dim
+    fdim = tdim - 1
+    domain.topology.create_connectivity(fdim, tdim)
+    boundary_facets = dolfinx.mesh.exterior_facet_indices(domain.topology)
+    boundary_dofs = dolfinx.fem.locate_dofs_topological(V, fdim, boundary_facets)
     
     u_d = dolfinx.fem.Function(V)
     u_d.interpolate(lambda x: np.full((x.shape[1],), Dirichlet_condition))
-    boundary_dofs = dolfinx.fem.locate_dofs_geometrical(V, boundary_Dirichlet)
+
     bc = dolfinx.fem.dirichletbc(u_d, boundary_dofs)
 
     problem = dolfinx.fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
@@ -67,7 +54,7 @@ def solve_test_problem(mesh_fname):
     #show_function(u, V)
 
     # ufl.sqrt(ufl.dot(ufl.grad(u), ufl.grad(u)))   ufl.exp(ufl.sqrt(ufl.dot(ufl.grad(u), ufl.grad(u))) ** 0.8)
-    weight_function_ufl = 0 + ufl.sqrt(ufl.dot(ufl.grad(u), ufl.grad(u))) ** 2
+    weight_function_ufl = 0.001 + ufl.sqrt(ufl.dot(ufl.grad(u), ufl.grad(u))) ** 2
     weight_function_expresion = dolfinx.fem.Expression(weight_function_ufl, V.element.interpolation_points())
     weight_function = dolfinx.fem.Function(V)
     weight_function.interpolate(weight_function_expresion)
@@ -128,6 +115,7 @@ def plot_function(fname, mesh_fname, triangulation, values):
     #plt.show()
     plt.close()
 
+
 def plot_function2(fname, mesh_fname, triangulation, values):
     gmsh.initialize()
     gmsh.open(mesh_fname)
@@ -153,32 +141,31 @@ def plot_function2(fname, mesh_fname, triangulation, values):
 def iterative_adaptation():
     n = 40
     niter = 100
+    # (0.1/4, 0.1/2, 0.1, 0.1*2, 0.1*4)
+    for j, d in enumerate((0.1/4, 0.1/2, 0.1, 0.1*2, 0.1*4), 1):
+        fname_mesh = f'meshes/iterative_adaptation_3_{j}'
+        fname_images = f'images/iterative_adaptation_3_{j}'
 
-    mesh_fname = 'meshes/test_problem.msh'
+        cur_mesh = f'{fname_mesh}_initial.msh'
+        adaptation_v2.adaptation(adaptation_v2.example, [lambda x: 1, lambda x: 1], n, niter, cur_mesh, run_gmsh=False)
 
-    fname = 'meshes/iterative_adaptation'
+        plotting.plot_triangle_mesh(f'{fname_images}_initial_mesh.pdf', cur_mesh, linewidth=1, markersize=0)
 
-    cur_mesh = f'{fname}_initial.msh'
-    adaptation_v2.adaptation(adaptation_v2.example, [lambda x: 1, lambda x: 1], n, niter, f'{fname}_initial.msh', run_gmsh=False)
+        for i in range(1, 5 + 1):
+            previous_mesh = cur_mesh
+            cur_mesh = f'{fname_mesh}_step_{i}.msh'
 
-    plotting.plot_triangle_mesh(f'images/iterative_adaptation_initial_mesh.pdf', cur_mesh, linewidth=1, markersize=0)
+            points, triangles, values_weight, values_solution = solve_test_problem(previous_mesh, d)
 
-    for i in range(1, 5 + 1):
-        previous_mesh = cur_mesh
-        cur_mesh = f'{fname}_step_{i}.msh'
+            triang = tri.Triangulation(points[:, 0], points[:, 1], triangles)
+            interpolator = tri.LinearTriInterpolator(triang, values_weight)
+            
+            plot_function(f'{fname_images}_step_{i}_function.pdf', previous_mesh, triang, values_weight)
+            plot_function2(f'{fname_images}_step_{i}_solution.pdf', previous_mesh, triang, values_solution)
 
-        points, triangles, values_weight, values_solution = solve_test_problem(previous_mesh)
-
-        triang = tri.Triangulation(points[:, 0], points[:, 1], triangles)
-        #interpolator = tri.CubicTriInterpolator(triang, values)
-        interpolator = tri.LinearTriInterpolator(triang, values_weight)
-        
-        plot_function(f'images/iterative_adaptation_step_{i}_function.pdf', previous_mesh, triang, values_weight)
-        plot_function2(f'images/iterative_adaptation_step_{i}_solution.pdf', previous_mesh, triang, values_solution)
-
-        wight_function = lambda x: interpolator(*x[:2])
-        adaptation_v2.adaptation(adaptation_v2.example, [wight_function, wight_function], n, niter, cur_mesh, run_gmsh=False)
-        plotting.plot_triangle_mesh(f'images/iterative_adaptation_step_{i}_mesh.pdf', cur_mesh, linewidth=1, markersize=0)
+            wight_function = lambda x: interpolator(*x[:2])
+            adaptation_v2.adaptation(adaptation_v2.example, [wight_function, wight_function], n, niter, cur_mesh, run_gmsh=False)
+            plotting.plot_triangle_mesh(f'{fname_images}_step_{i}_mesh.pdf', cur_mesh, linewidth=1, markersize=0)
 
 
 if __name__ == '__main__':
@@ -190,23 +177,23 @@ if __name__ == '__main__':
     niter = 100
 
     mesh_fname = 'meshes/test_problem.msh'
+    #mesh_fname = 'meshes/iterative_adaptation_2_step_5.msh'
 
-    adaptation_v2.adaptation(adaptation_v2.example, [lambda x: 1, lambda x: 1], n, niter, mesh_fname, run_gmsh=False)
+    #adaptation_v2.adaptation(adaptation_v2.example, [lambda x: 1, lambda x: 1], n, niter, mesh_fname, run_gmsh=False)
 
-    points, triangles, values = solve_test_problem(mesh_fname)
+    points, triangles, values_weight, values_solution = solve_test_problem(mesh_fname)
 
     triang = tri.Triangulation(points[:, 0], points[:, 1], triangles)
-    #interpolator = tri.CubicTriInterpolator(triang, values)
-    interpolator = tri.LinearTriInterpolator(triang, values)
+    interpolator = tri.LinearTriInterpolator(triang, values_weight)
 
     plotting.plot_triangle_mesh('images/test_mesh.pdf', mesh_fname, linewidth=1, markersize=0)
-    plot_function('images/test_function.pdf', mesh_fname, triang, values)
+    plot_function('images/test_function.pdf', mesh_fname, triang, values_weight)
 
-    #exit()
+    exit()
 
     wight_function = lambda x: interpolator(*x[:2])
     result_mesh_fname = 'meshes/test_problem_res.msh'
-    adaptation_v2.adaptation(adaptation_v2.example, [wight_function, wight_function], n, niter, result_mesh_fname, run_gmsh=True)
+    adaptation_v2.adaptation(adaptation_v2.example, [wight_function, wight_function], n, niter, result_mesh_fname, run_gmsh=False)
     plotting.plot_triangle_mesh('images/test_mesh_res.pdf', result_mesh_fname, linewidth=1, markersize=0)
 
     
